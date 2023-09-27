84a742ffb0b0f5ea9e4ade8957c1e9c9c732c206
edlymulyaandeslin4 <edlymulyaandeslin@gmail.com>
2023-09-27 13:58:39 +0700

    upload file tugas php-2

---
+++ function-conditional.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Berlatih function</title>
+</head>
+
+<body>
+    <h1>Berlatih Function PHP</h1>
+    <?php
+    echo "<h3> Soal No 1 Greetings</h3>";
+    /*Soal No 1
+    Greetings Buatlah sebuah function greetings() yang menerima satu parameter berupa string. contoh: greetings("abduh"); Output: "Halo Abduh, Selamat Datang di Garuda Cyber Institute!" */
+    // Code function di sini
+    // Hapus komentar untuk menjalankan code!
+    function greetings($str)
+    {
+        echo 'Halo ' . $str . ', Selamat Datang di Garuda Cyber Institute!';
+        echo '<br/>';
+    }
+    greetings("Bagas");
+    greetings("Wahyu");
+    greetings("Edly Mulya Andeslin");
+
+    echo "<h3>Soal No 2 Reverse String</h3>";
+    /*Soal No 2
+    Reverse String
+    Buatlah sebuah function reverseString() untuk mengubah string berikut menjadi kebalikannya menggunakan function dan looping (for/while/do while). Function reverseString menerima satu parameter berupa string. 
+    NB: DILARANG menggunakan built-in function PHP sepert strrev(), HANYA gunakan LOOPING! reverseString("abdul"); 
+    Output: ludba
+    */
+    // Code function di sini
+    // Hapus komentar di bawah ini untuk jalankan Code
+
+    function reverseString($str)
+    {
+        $reverse = '';
+        for ($i = strlen($str) - 1; $i >= 0; $i--) {
+            $reverse .= $str[$i];
+        }
+        return $reverse;
+    }
+    // result
+    echo reverseString("Edly Mulya Andeslin");
+    echo '<br/>';
+    echo reverseString("Garuda Cyber Institute");
+    echo '<br/>';
+    echo reverseString("We Are GC-Ins Developer");
+
+
+    echo "<h3>Soal No 3 Palindrome </h3>";
+    /*Soal No 3
+    Palindrome Buatlah sebuah function yang menerima parameter berupa string yang mengecek apakah string tersebut sebuah palindrome atau bukan. Palindrome adalah sebuah kata atau kalimat yang jika dibalik akan memberikan kata yang sama contohnya: katak, civic. Jika string tersebut palindrome maka akan mengembalikan nilai true, sedangkan jika bukan palindrome akan mengembalikan false. 
+    NB: 
+    Contoh: palindrome("katak") => 
+    output : "true" 
+    palindrome("jambu") => 
+    output : "false" 
+    NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan looping seperti biasa atau gunakan function reverseString dari jawaban no.2!    
+    */
+
+    // Code function di sini
+    function palindrome($str)
+    {
+        $revers = reverseString($str);
+        if ($str == $revers) {
+            return $str . ' = true';
+        } else {
+            return $str . ' = false';
+        }
+    }
+    // Hapus komentar di bawah ini untuk jalankan code
+    echo palindrome('civic'); // true
+    echo '<br/>';
+    echo palindrome("nababan"); // true
+    echo '<br/>';
+    echo palindrome("jambaban"); // false
+    echo '<br/>';
+    echo palindrome("racecar"); // true
+
+
+    echo "<h3>Soal No 4 Tentukan Nilai </h3>";
+    /*Soal 4
+    buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter berupa integer. dengan ketentuan 
+    jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” Selain itu 
+    jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu 
+    jika parameter number lebih besar sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang” */
+    // Code function di sini
+    function tentukan_nilai($int)
+    {
+        if ($int >= 85 && $int <= 100) {
+            return 'Sangat baik';
+        } else if ($int >= 70 && $int < 85) {
+            return 'Baik';
+        } else if ($int >= 60 && $int < 70) {
+            return 'Cukup';
+        } else {
+            return 'Kurang';
+        }
+    }
+    // Hapus komentar di bawah ini untuk jalankan code
+    echo tentukan_nilai(98); //Sangat Baik
+    echo '<br/>';
+    echo tentukan_nilai(76); //Baik
+    echo '<br/>';
+    echo tentukan_nilai(67); //Cukup
+    echo '<br/>';
+    echo tentukan_nilai(43); //Kurang
+
+    ?>
+
+</body>
+
+</html>
